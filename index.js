a0cda12416f07a4a221d8a565a05f7386c8c53d5
Avinash <avinashkumarsinha01@gmail.com>
2024-08-24 01:52:15 +0530

    Add cors

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const corsOptions = {
-    origin: 'https://example.com', 
-    methods: 'GET,POST,PUT,DELETE', 
-    allowedHeaders: 'Content-Type,Authorization', 
+    origin: '*',  
+    Credential: true,
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE","OPTIONS"],
+    allowedHeaders: 'Content-Type, Authorization',
 };
 
+
+
 app.use(cors(corsOptions));
-
-
 app.use(bodyParser.json());
 app.use('/api', schoolRoutes);
 
