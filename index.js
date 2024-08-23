7d20b5710b913094c7d89075ed7f4cf5e727a900
Avinash <avinashkumarsinha01@gmail.com>
2024-08-24 01:45:19 +0530

    Add cors

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const schoolRoutes = require('./src/routes/schoolRoutes');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const corsOptions = {
+    origin: 'https://example.com', 
+    methods: 'GET,POST,PUT,DELETE', 
+    allowedHeaders: 'Content-Type,Authorization', 
+};
+
+app.use(cors(corsOptions));
+
 
 app.use(bodyParser.json());
 app.use('/api', schoolRoutes);
